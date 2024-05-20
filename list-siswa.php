ed93a3540088e0b4b9fac3aabde5426838c3380d
LAPTOP-UPG5JS3M\User <anindyaputri231204@gmail.com>
2024-05-20 10:06:33 +0700

    Tugas WEB menggunakan library bootstrap

--- list-siswa.php
+++ list-siswa.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pendaftaran Siswa Baru | PNM</title>
+    <title>Pendaftaran Siswa Baru </title>
     <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
     <style>
           body {
